c376b2cce9f5e1a219123f7fdc670b710cfd1abb
cavazosdotcom <cavazosdotcom@gmail.com>
2022-02-15 16:27:32 -0800

    added routes for category-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,4 +1,4 @@
-  const router = require('express').Router();
+const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 const { findAll } = require('../../models/Product');
 
@@ -109,8 +109,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
